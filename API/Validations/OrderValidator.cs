2f2e4a15919be3db1fa5bb7b9decc4a7c5e22b5a
Luis Lopez <luis.dev.master@gmail.com>
2023-08-16 02:03:34 -0600

    Creating rule for pricing

--- API/Validations/OrderValidator.cs
+++ API/Validations/OrderValidator.cs
@@ -7,11 +7,16 @@ public class OrderValidator : AbstractValidator<Order>
 {
     public OrderValidator()
     {
-        // Rules for CusomerName field
+        // Rules for CustomerName field
         RuleFor(model => model.CustomerName).NotNull();
         RuleFor(model => model.CustomerName).NotEmpty();
         RuleFor(model => model.CustomerName).MaximumLength(10);
         RuleFor(model => model.CustomerName).MinimumLength(5);
         
+        // Rules for Price field
+        RuleFor(model => model.Price).InclusiveBetween(500, 1500);
+        
+        
+
     } 
 }
