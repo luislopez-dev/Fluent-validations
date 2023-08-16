228b976f5c4f93ee90b927889ed0b250044b9563
Luis Lopez <luis.dev.master@gmail.com>
2023-08-16 02:14:55 -0600

    Adding validator for orderstatus field

--- API/Validations/OrderValidator.cs
+++ API/Validations/OrderValidator.cs
@@ -13,16 +13,22 @@ public class OrderValidator : AbstractValidator<Order>
         RuleFor(model => model.CustomerName).MaximumLength(10);
         RuleFor(model => model.CustomerName).MinimumLength(5);
         
-        // Rules for Price field
+        // Rules for Price Field
         RuleFor(model => model.Price).InclusiveBetween(500, 1500);
         
-        // Rules for Customer Email
+        // Rules for CustomerEmail field
         /*
          * This email validator validates the customer email address
          * against built in regular expressions which cover most of the
          * email address formats
          */
         RuleFor(model => model.CustomerEmail).EmailAddress();
-
+        
+        // Rules for OrderStatus field
+        /*
+         * This validator will ensure that the value
+         * is one of the included members of the enum
+         */ 
+        RuleFor(model => model.OrderStatus).IsInEnum();
     } 
 }
