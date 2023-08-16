6c5c1d7392c029f616bcbd26457856790be22b2a
Luis Lopez <luis.dev.master@gmail.com>
2023-08-16 03:59:17 -0600

    Creating FullName extension method for Fluent Validations

--- API/Extensions/FluentValidationExtensions.cs
+++ API/Extensions/FluentValidationExtensions.cs
@@ -1,6 +1,13 @@
-﻿namespace API.Extensions;
+﻿using FluentValidation;
 
-public class FluentValidationExtensions
+namespace API.Extensions;
+
+public static class FluentValidationExtensions
 {
-    
+    public static IRuleBuilderOptions<T, string> FullName<T>(this IRuleBuilder<T, string> ruleBuilder)
+    {
+        return ruleBuilder
+            .MinimumLength(10)
+            .Must(val => val.Split(" ").Length >= 2);
+    }
 }
